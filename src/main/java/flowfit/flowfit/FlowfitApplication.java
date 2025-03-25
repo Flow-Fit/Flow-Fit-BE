98d179abbdf95a6d756900d7b7f2f3906d6f7c6c
chltmdgh522 <chltmdgh517@naver.com>
2025-03-26 00:48:17 +0900

    Feat: 기초 공사1

---
+++ src/main/java/flowfit/flowfit/FlowfitApplication.java
@@ -0,0 +1,13 @@
+package flowfit.flowfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowfitApplication.class, args);
+	}
+
+}
