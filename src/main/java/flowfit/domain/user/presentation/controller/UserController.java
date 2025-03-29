95356cdc58f8d38f96237c5715e958d6f2911ed4
chltmdgh522 <chltmdgh517@naver.com>
2025-03-30 00:39:51 +0900

    Fix: postman 토큰 테스트 완료

--- src/main/java/flowfit/domain/user/presentation/controller/UserController.java
+++ src/main/java/flowfit/domain/user/presentation/controller/UserController.java
@@ -2,11 +2,14 @@ package flowfit.domain.user.presentation.controller;
 
 import flowfit.domain.user.application.service.UserService;
 import flowfit.domain.user.domain.entity.User;
+import flowfit.domain.user.domain.repository.UserRepository;
 import flowfit.domain.user.presentation.dto.req.UserJoinDto;
 import flowfit.domain.user.presentation.dto.req.UserLoginDto;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -16,9 +19,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
 
+    private final UserRepository userRepository;
     @PostMapping("/join")
     public void UserJoin(@RequestBody UserJoinDto joinDto, HttpServletResponse response) throws IOException {
         userService.userJoin(joinDto, response);
@@ -28,5 +33,8 @@ public class UserController {
     public void UserJoin(@RequestBody UserLoginDto loginDto, HttpServletResponse response) throws IOException {
         userService.userLoin(loginDto,response);
     }
+
+
+
 
 }
