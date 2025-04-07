69bd2a621794b01b42212e69e9e236c9a9c781b8
chan000518 <wkdvnddl050701@gmail.com>
2025-04-07 17:18:55 +0900

    chore: 스웨거 jwt등록

--- src/main/java/flowfit/global/config/SwaggerConfig.java
+++ src/main/java/flowfit/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package flowfit.global.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -15,6 +17,12 @@ import org.springframework.context.annotation.Configuration;
                         email = "chltmdgh517@naver.com"
                 )
         )
+)
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 @Configuration
 public class SwaggerConfig {
