b725d3c1ab510d2a96400b0d50ab7dd83d23ef15
chan000518 <wkdvnddl050701@gmail.com>
2025-04-07 17:20:01 +0900

    fix: 로그인 시 "type" 파라미터로 수정
    로그인 시 "type"을 바디 대신 파라미터로 받게 수정

--- src/main/java/flowfit/domain/oauth2/presentation/controller/LoginLinkController.java
+++ src/main/java/flowfit/domain/oauth2/presentation/controller/LoginLinkController.java
@@ -4,10 +4,7 @@ import flowfit.domain.oauth2.application.service.LoginLinkService;
 import flowfit.domain.oauth2.presentation.dto.request.LoginRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/oauth2")
@@ -17,7 +14,7 @@ public class LoginLinkController {
     private final LoginLinkService loginLinkService;
 
     @GetMapping("/login")
-    public ResponseEntity<String> loginPage(@RequestBody LoginRequestDTO requestDTO) {
-        return ResponseEntity.status(200).body(loginLinkService.getLoginLink(requestDTO.type()));
+    public ResponseEntity<String> loginPage(@RequestParam("type") String type) {
+        return ResponseEntity.status(200).body(loginLinkService.getLoginLink(type));
     }
 }
